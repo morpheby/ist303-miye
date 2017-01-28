d6031ef96ea5b2d4ab68b0d09706337b7c58150f
Ilya Mikhaltsou <morpheby@gmail.com>
2017-01-27 21:30:30 -0800

    Optimized appveyor version scripts and allowed tagged builds to bintray

--- tools/appveyor-version.sh
+++ tools/appveyor-version.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-appveyor UpdateBuild -Version `tools/versionGen.sh ${APPVEYOR_BUILD_NUMBER}`
+appveyor UpdateBuild -Version $(. tools/versionGen.sh ${APPVEYOR_BUILD_NUMBER})
+appveyor SetVariable -Name GIT_TAG -Value $(git describe --abbrev=0)
+appveyor SetVariable -Name VERSION_SHORT -Value $(. tools/versionGen.sh -s)
--- tools/gittag.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`git describe "--abbrev=0" `) DO (
-SET GIT_TAG=%%F
-)
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`sh tools\versionGen.sh -s`) DO (
-SET VERSION_SHORT=%%F
-)
