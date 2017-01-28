36812e4c786c91381372a8d94aeeadea007fd552
Ilya Mikhaltsou <morpheby@gmail.com>
2017-01-27 23:34:29 -0800

    FIXUP: Enabled proper versioning on appveyor

--- tools/appveyor-version.sh
+++ tools/appveyor-version.sh
@@ -12,7 +12,7 @@ echo "VERSION_SHORT=$VERSION_SHORT"
 GIT_TAG=$(git describe --abbrev=0)
 echo "GIT_TAG=$GIT_TAG"
 
-appveyor UpdateBuild -Version "BUILD_VERSION"
+appveyor UpdateBuild -Version "$BUILD_VERSION"
 appveyor SetVariable -Name VERSION_FULL -Value "$VERSION_FULL"
 appveyor SetVariable -Name GIT_TAG -Value "$GIT_TAG"
 appveyor SetVariable -Name VERSION_SHORT -Value "$VERSION_SHORT"
