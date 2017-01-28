35f591b5caa6088aaf07e5db74fb51501550e0f4
Ilya Mikhaltsou <morpheby@gmail.com>
2017-01-27 19:09:49 -0800

    Updated versiongen.cmd

--- tools/versionGen.cmd
+++ tools/versionGen.cmd
@@ -1,16 +1,24 @@
 @echo off
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`git describe --abbrev=0`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`git describe "--abbrev=0" `) DO (
 SET GIT_TAG=%%F
 )
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list %GIT_TAG%..HEAD | find /v /c "" `) DO (
+git rev-list %GIT_TAG%..HEAD | wc -l | tr -d ' ' > tmp-versiongen-gittagdata.txt
+
+FOR /F "tokens=*" %%F IN (tmp-versiongen-gittagdata.txt) DO (
 SET GIT_COMMIT_COUNT=%%F
 )
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list HEAD | find /v /c "" `) DO (
+del tmp-versiongen-gittagdata.txt
+
+git rev-list HEAD | wc -l | tr -d ' ' > tmp-versiongen-gittagdata.txt
+
+FOR /F "tokens=*" %%F IN (tmp-versiongen-gittagdata.txt) DO (
 SET GIT_TOTAL_COMMIT_COUNT=%%F
 )
+
+del tmp-versiongen-gittagdata.txt
 
 IF %GIT_COMMIT_COUNT%==0 (
 	echo %GIT_TAG%.%GIT_COMMIT_COUNT%
