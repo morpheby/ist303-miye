00a708fcd9d74a685db77e18df7930b20dd70a5b
Ilya Mikhaltsou <morpheby@gmail.com>
2017-01-27 17:58:57 -0800

    Modified versioning to include build numbers

--- tools/versionGen.cmd
+++ tools/versionGen.cmd
@@ -4,8 +4,22 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`git describe --abbrev=0`) DO (
 SET GIT_TAG=%%F
 )
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list %GIT_TAG%..HEAD | wc -l | tr -d ' '`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list %GIT_TAG%..HEAD | find /v /c "" `) DO (
 SET GIT_COMMIT_COUNT=%%F
 )
 
-echo %GIT_TAG%.%GIT_COMMIT_COUNT%
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list HEAD | find /v /c "" `) DO (
+SET GIT_TOTAL_COMMIT_COUNT=%%F
+)
+
+IF %GIT_COMMIT_COUNT%==0 (
+	echo %GIT_TAG%.%GIT_COMMIT_COUNT%
+) ELSE (
+	IF NOT DEFINED APPVEYOR_BUILD_NUMBER (
+		set BUILD=%GIT_TOTAL_COMMIT_COUNT%
+	) ELSE (
+		set BUILD=%APPVEYOR_BUILD_NUMBER%
+	)
+	echo %GIT_TAG%.%GIT_COMMIT_COUNT%+%BUILD%
+)
+
--- tools/versionGen.sh
+++ tools/versionGen.sh
@@ -15,5 +15,14 @@ GIT_COMMIT_COUNT=`git rev-list ${GIT_TAG}..HEAD | wc -l | tr -d ' '`
 GIT_TOTAL_COMMIT_COUNT=`git rev-list HEAD | wc -l | tr -d ' '`
 RELEASE_VERSION=`git describe`
 
-echo ${GIT_TAG}.${GIT_COMMIT_COUNT}
+if [[ ${GIT_COMMIT_COUNT} == 0 ]]; then
+    echo ${GIT_TAG}.${GIT_COMMIT_COUNT}
+else
+    if [[ -z ${TRAVIS_BUILD_NUMBER} ]]; then
+        BUILD=${GIT_TOTAL_COMMIT_COUNT}
+    else
+        BUILD=${TRAVIS_BUILD_NUMBER}
+    fi
+    echo ${GIT_TAG}.${GIT_COMMIT_COUNT}+${BUILD}
+fi
 
