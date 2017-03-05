d0752c3b364e231c088292b8fb4fc88f7f147d8d
damapak <dkallemeyn@yahoo.com>
2017-03-04 22:19:20 -0800

    added cost calculation to reservation.  moved vars to reservation class

--- models/Repository.py
+++ models/Repository.py
@@ -44,10 +44,3 @@ class Repository(object):
     def find_reservations_checked(self, c_in, c_out):
         found = [r for r in self.reservations if r.checked_in == c_in and r.checked_out == c_out]
         return found
-
-
-#Static HOTEL variables
-# base prices are weekday, off-season
-hotel_baseprice = {'Single': 140, 'Double': 260, 'Quad': 480}
-peak_multiplier = 1.25 # peak season: 5/15-8/15 & 12/15-1/15
-weekend_surcharge = 30  #weekends are $30 more than base price (applied after peak mult)
