2427a67394f1e72f6be2a190255304858206208c
damapak <dkallemeyn@yahoo.com>
2017-03-05 15:58:47 -0800

    created list var of possible spa res timeslots

--- models/SpaRes.py
+++ models/SpaRes.py
@@ -1,5 +1,5 @@
 class SpaRes:
-
+  
   def __init__(self, personID, date, time, treatment_type):
     self.person = personID
     self.date = date
@@ -17,4 +17,28 @@ class SpaRes:
 
   def print_price_menu():
     pass
+
+
+#SPA STATIC VARS
+#available time slots
+timeslots_military = [(x + 5)/10 for x in range(75,200,5)]
+timeslots = [] #human readable list of time slots, i.e. 3:00 PM instead of 14
+for timeval in timeslots_military:
+  hour_min = divmod(timeval,1)
+  if timeval < 12:
+    ampm = 'AM'
+    hour = int(hour_min[0])
+  elif timeval == 12 or timeval == 12.5:
+    ampm = 'PM'
+    hour = int(hour_min[0])
+  else:
+    ampm = 'PM'
+    hour = int(hour_min[0]-12)
+  min = int(hour_min[1]*60)
+  min = str(min)
+  if min == '0':
+    min = '00'
+  timeslots.append(str(hour) + ":" + min + " " + ampm)
+
+
 
