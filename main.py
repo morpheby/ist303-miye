f01800029c9f59858a9e39f061bb80956b216974
Ilya Mikhaltsou <morpheby@gmail.com>
2017-05-04 16:03:51 -0700

    Added directions to miye in system

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def create_app_config():
     config.add_route('view_bill', '/bill/{res_id}')
     config.add_route('view_checkin_confirmation', '/checkin/{res_id}')
     config.add_route('spa_reservation', '/spa_reservation')
+    config.add_route('directions', '/directions')
     
     config.scan(controllers)
     
